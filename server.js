39ddec211caec986dd0e3214cfd3e87ae79bf6f5
avix101 <stashablank@gmail.com>
2017-10-02 13:56:09 -0400

    Simple Server Structure

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+const port = process.env.PORT || 5000;
+
+const views = __dirname + "/views/";
+app.use(express.static(views));
+
+app.get("/", function(request, response/*, next */) {
+
+	response.send("index.html");
+});
+
+server.listen(port, function(){
+	console.log("herd.io launched...");
+});
